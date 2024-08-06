0e5c777f536bfffa15bf93ce24e8eecca49934c4
Tim Neale <neale.timw@gmail.com>
2024-08-06 14:02:31 -0400

    things mostly work now, still has issue of clicking and ticking totals

--- src/routes/clicker/Clicker.js
+++ src/routes/clicker/Clicker.js
@@ -1,22 +1,54 @@
 import React, {useEffect, useState} from 'react';
+import { painArray, timeArray } from './SpeechBubbles';
 
 import './Clicker.css';
 
 function Clicker() {
   const [total, setTotal] = useState(0)
-  const [upgrades, setUpgrades] = useState([{name:'Arrow', price: 5, cps:0.2}])
+  const [lifetimeTotal, setLifetime] = useState(0)
+  const [milestone, setMilestone] = useState(10)
+  const [upgrades, setUpgrades] = useState([{name:'Lillia Passive', price: 5, cps:0.2, unlockNumber: 5, image:'LilliaP.webp'}])
   const [clicksPerSec, setClicksPerSec] = useState(0)
   const [owned, setOwned] = useState({})
   const [sessionTime, setSessionTime] = useState(0)
+  const [speechText, setSpeechText] = useState('0')
 
-  useEffect(() => {
-
-  }, [total])
-
+  const handleUnlocks = () => {
+    const notUnlocked = upgrades.find(t => !t.unlocked);
+    if(notUnlocked?.unlockNumber <= total){
+      const newUp = [...upgrades]
+      const index = newUp.findIndex(t => t.name === notUnlocked.name)
+      if(index !== undefined){
+        newUp[index] = {...newUp[index], unlocked: true}
+      }
+      setUpgrades(newUp)
+    }
+  }
   const autoClick = () => {
     setTotal(Math.round((total + clicksPerSec) * 10)/10);
+    setLifetime(Math.round((lifetimeTotal + clicksPerSec) * 10)/10);
     setSessionTime(sessionTime + 1);
   }
+
+  useEffect(() => {
+    handleUnlocks();
+    if(lifetimeTotal >= milestone){
+      const speechIndex = Math.floor(Math.random() * painArray?.length)
+      setSpeechText(painArray[speechIndex])
+      setMilestone(milestone * 5)
+    }
+    else if(sessionTime && !(sessionTime % 60)){
+      const speechIndex = Math.floor(Math.random() * timeArray?.length)
+      setSpeechText(timeArray[speechIndex])
+    }
+  }, [total])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSpeechText('0')
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, [speechText])
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       autoClick()
@@ -30,6 +62,7 @@ function Clicker() {
 
   const deerClick = () => {
     setTotal(total + 1)
+    setLifetime(lifetimeTotal + 1)
   }
 
   const calculateClicks = () => {
@@ -53,27 +86,28 @@ function Clicker() {
     setTotal(Math.round((total - newPrice) * 10)/10)
     setOwned({...owned, [upgrade.name]:(owned[upgrade.name] ?? 0) + 1})
   }
-  console.log(total)
 
   return (
     <div className="deerApp">
       <div className='deerContainer'>
+        <div class={speechText.length > 1 ? "speech-bubble" : "speech-bubble-hidden"}>{speechText}</div>
         <a className='deerButton' onClick={deerClick}>
-          <img alt='Deer' className='deer' src={require('./../../images/deer.png')} />
+          <img draggable='false' alt='Deer' className='deer' src={require('./../../images/deer.png')} />
         </a>
         <p>You've clicked the deer {total} times!</p>
         <br/>
-        {clicksPerSec ? <p>Generating {clicksPerSec} clicks per second</p> : <></>}
+        {clicksPerSec ? <p>Generating {clicksPerSec} clicks per second</p> : <p></p>}
       </div>
       <div className='upgradeContainer'>
         <ul className='upgradeList'>
-          {upgrades.map(u =>{
+          {upgrades.filter(t => t.unlocked).map(u =>{
             const newPrice = Math.round(u.price * (owned[u.name] ? 1.1 * owned[u.name] : 1))
-            return (<button className='upgrade' onClick={() =>
+            return (<div className={total < newPrice ? 'upgradeDisabled' : 'upgrade'} onClick={() =>
                       buyUpgrade(u, newPrice)
                     }>
-                      {u.name} - {newPrice} {owned[u.name] ? `(${owned[u.name]} owned)` : ''}
-                    </button>)
+                      <img draggable='false' src={u.image ? require(`./../../images/upgradeImages/${u.image}`) : ''} className={total < newPrice ? 'disabledUpgradeImage' : 'upgradeImage'}/>
+                      <span>{u.name} - {newPrice} {owned[u.name] ? `(${owned[u.name]} owned)` : ''}</span>
+                    </div>)
             }
           )}
         </ul>
---
+++ src/routes/clicker/SpeechBubbles.js
@@ -0,0 +1,8 @@
+export const painArray = [
+    'oof ouch owie',
+    'Why are you like this?'
+]
+export const timeArray = [
+    `You've been here a while`,
+    `Not bored yet?`
+]
