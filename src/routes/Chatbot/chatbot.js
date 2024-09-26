19a57d6074fca8fbfeaf7eab522336453addfcf7
Tim Neale <neale.timw@gmail.com>
2024-09-26 18:00:02 -0400

    make pretty transition

--- src/routes/Chatbot/chatbot.js
+++ src/routes/Chatbot/chatbot.js
@@ -8,10 +8,11 @@ function Chatbot() {
     const [prompts, setPrompts] = useState([]);
     const [shownPrompts, setShownPrompts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [hidden, setHidden] = useState(false);
     const [msgNum, setMsgNum] = useState(1);
     const [error, setError] = useState("");
     const maxPrompts = 12;
-    const messagesEndRef = useRef()
+    const messagesEndRef = useRef();
 
     const input = document.getElementById("textbox");
     input?.addEventListener("keypress", function (event) {
@@ -34,10 +35,12 @@ function Chatbot() {
         setLoading(true);
         sendTextToApi({ text: currentText, msgNum, messages: prompts })
             .then((r) => {
+                setHidden(true);
                 setMsgNum(r.msgNum);
                 setText("");
                 setPrompts(r.chat);
                 setLoading(false);
+                setTimeout(() => setHidden(false), 100);
             })
             .catch((e) => setError(e));
     };
@@ -56,108 +59,110 @@ function Chatbot() {
                 )
                 .filter((p) => p.role !== "system")
         );
-        setTimeout( () => {
-            messagesEndRef?.current?.scrollIntoView({behavior:"smooth"})
+        setTimeout(() => {
+            messagesEndRef?.current?.scrollIntoView({ behavior: "smooth" });
         }, 100);
     }, [prompts]);
 
     return (
         <div className="container">
             <img src={require("./clouds.jpg")} className="backgroundImage" />
-                <div className="contentThing">
-                    <div id="prompts" className="prompts">
-                        {shownPrompts.map((p, i) =>
-                            i === shownPrompts.length - 1 ? (
-                                <p className="currentPrompt">
-                                    {p.content.split("\n").map((char, index) => {
-                                        const style = {
-                                            "animation-delay":
-                                                0.5 + index / maxPrompts + "s",
-                                        };
-                                        return (
-                                            <p
-                                                style={style}
-                                                aria-hidden="true"
-                                                key={index}
+            <div className="contentThing">
+                <div id="prompts" className="prompts">
+                    {shownPrompts.map((p, i) =>
+                        i === shownPrompts.length - 1 ? (
+                            <p className="currentPrompt">
+                                {p.content.split("\n").map((line, index) => (
+                                    <p aria-hidden="true" key={index}>
+                                        {[...line].map((char, charIndex) => (
+                                            <span
+                                                style={{
+                                                    opacity: hidden ? 0 : 1,
+                                                    transition: `${
+                                                        0.1 * charIndex +
+                                                        0.1 +
+                                                        index * 2
+                                                    }s`,
+                                                }}
                                             >
                                                 {char}
-                                            </p>
-                                        );
-                                    })}
-                                </p>
-                            ) : (
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        justifyContent:
-                                            p.role === "user" ? "end" : "center",
-                                    }}
-                                >
-                                    {p.role === "user" ? (
-                                        <img
-                                            className="replyArrow"
-                                            style={{
-                                                opacity: `${
-                                                    (100 -
-                                                        (100 / maxPrompts) *
-                                                            (shownPrompts.length -
-                                                                i)) /
-                                                    2
-                                                }%`,
-                                            }}
-                                            src={require("./reply.png")}
-                                        />
-                                    ) : (
-                                        <></>
-                                    )}
-                                    <p
-                                        className={
-                                            p.role === "user"
-                                                ? "pastUserPrompt"
-                                                : "pastAssistPrompts"
-                                        }
+                                            </span>
+                                        ))}
+                                    </p>
+                                ))}
+                            </p>
+                        ) : (
+                            <div
+                                style={{
+                                    display: "flex",
+                                    justifyContent:
+                                        p.role === "user" ? "end" : "center",
+                                }}
+                            >
+                                {p.role === "user" ? (
+                                    <img
+                                        className="replyArrow"
                                         style={{
                                             opacity: `${
-                                                100 -
-                                                (100 / maxPrompts) *
-                                                    (shownPrompts.length - i)
+                                                (100 -
+                                                    (100 / maxPrompts) *
+                                                        (shownPrompts.length - i)) /
+                                                2
                                             }%`,
                                         }}
-                                    >
-                                        {p.content}
-                                    </p>
-                                </div>
-                            )
-                        )}
-                        <div id="bottom" ref={messagesEndRef}/>
-                    </div>
-
-                    <div className="inputArea">
-                        <input
-                            id="textbox"
-                            type="text"
-                            value={currentText}
-                            className="textboxThing"
-                            onChange={(e) => setText(e.target.value)}
-                            disabled={loading}
-                        />
-                        <input
-                            id="sendButton"
-                            type="button"
-                            className="sendButton"
-                            onClick={sendText}
-                            value={"Send"}
-                            disabled={loading}
-                        />
-                    </div>
-                    {error.length ? <p className="error">{error}</p> : <></>}
+                                        src={require("./reply.png")}
+                                    />
+                                ) : (
+                                    <></>
+                                )}
+                                <p
+                                    className={
+                                        p.role === "user"
+                                            ? "pastUserPrompt"
+                                            : "pastAssistPrompts"
+                                    }
+                                    style={{
+                                        opacity: `${
+                                            100 -
+                                            (100 / maxPrompts) *
+                                                (shownPrompts.length - i)
+                                        }%`,
+                                    }}
+                                >
+                                    {p.content}
+                                </p>
+                            </div>
+                        )
+                    )}
+                    <div id="bottom" ref={messagesEndRef} />
                 </div>
-                <span className="signature">
-                    a dumb thing made by{" "}
-                    <a className="signatureLink" href="https://nealetw.com/">
-                        Tim Neale
-                    </a>
-                </span>
+
+                <div className="inputArea">
+                    <input
+                        id="textbox"
+                        type="text"
+                        value={currentText}
+                        className="textboxThing"
+                        onChange={(e) => setText(e.target.value)}
+                        disabled={loading}
+                    />
+                    <input
+                        id="sendButton"
+                        type="button"
+                        className="sendButton"
+                        onClick={sendText}
+                        value={"Send"}
+                        disabled={loading}
+                    />
+                </div>
+                {error.length ? <p className="error">{error}</p> : <></>}
+            </div>
+            <span className="signature">
+                a dumb thing made by{" "}
+                <a className="signatureLink" href="https://nealetw.com/">
+                    Tim Neale
+                </a>
+            </span>
         </div>
     );
 }
